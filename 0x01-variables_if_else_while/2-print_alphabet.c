a901b33791e01a07d3d2e32e3bef2b3ea877e3f8
Mahmoud Sobhy Aboelezz <73950001+mahmoudaboelezz@users.noreply.github.com>
2023-03-21 23:33:56 +0200

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+    char c;
+    for (i = 0; i < 26; i++)
+    {
+        c = 'a' + i;
+        putchar(c);
+    }
+}
+
